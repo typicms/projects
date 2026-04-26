2184b218f6ac55ec03c8b5ca4ded4ee233a8386f
Samuel De Backer <sdebacker@gmail.com>
2026-04-26 22:18:45 +0200

    Draggable table rows

--- resources/views/admin/projects/index-categories.blade.php
+++ resources/views/admin/projects/index-categories.blade.php
@@ -3,7 +3,7 @@
 @section('title', __('Project categories'))
 
 @section('content')
-    <item-list url-base="/api/projects/categories" fields="id,image_id,position,status,title" table="project_categories" title="categories" include="image" :searchable="['title']" :sorting="['position']">
+    <item-list url-base="/api/projects/categories" fields="id,image_id,position,status,title" table="project_categories" title="categories" include="image" :searchable="['title']" :sorting="['position']" :draggable="$can('update project_categories')">
         <template #back-button>
             <x-core::back-button :back-url="route('admin::index-projects')" :back-label="__('Projects')" />
         </template>
@@ -13,15 +13,18 @@
         </template>
 
         <template #columns="{ sortArray }">
+            <item-list-column-header name="position" sortable :sort-array="sortArray" :label="$t('Order')"></item-list-column-header>
             <item-list-column-header name="checkbox" v-if="$can('update project_categories')||$can('delete project_categories')"></item-list-column-header>
             <item-list-column-header name="edit" v-if="$can('update project_categories')"></item-list-column-header>
             <item-list-column-header name="status_translated" sortable :sort-array="sortArray" :label="$t('Status')"></item-list-column-header>
-            <item-list-column-header name="position" sortable :sort-array="sortArray" :label="$t('Position')"></item-list-column-header>
             <item-list-column-header name="image" :label="$t('Image')"></item-list-column-header>
             <item-list-column-header name="title_translated" sortable :sort-array="sortArray" :label="$t('Title')"></item-list-column-header>
         </template>
 
-        <template #table-row="{ model, checkedModels, loading }">
+        <template #table-row="{ model, checkedModels, loading, sortArray }">
+            <td class="drag-handle text-muted" v-if="$can('update partners')" :style="{ cursor: sortArray[0] === 'position' ? 'grab' : 'default' }">
+                <i :class="['icon-grip-vertical', { 'opacity-50': sortArray[0] !== 'position' }]"></i>
+            </td>
             <td class="checkbox" v-if="$can('update project_categories')||$can('delete project_categories')">
                 <item-list-checkbox :model="model" :checked-models-prop="checkedModels" :loading="loading"></item-list-checkbox>
             </td>
@@ -30,9 +33,6 @@
             </td>
             <td>
                 <item-list-status-button :model="model"></item-list-status-button>
-            </td>
-            <td>
-                <item-list-position-input :model="model"></item-list-position-input>
             </td>
             <td><img :src="model.thumb" alt="" height="27" /></td>
             <td>@{{ model.title_translated }}</td>
